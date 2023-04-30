81ade30a119913634aa2a9fd8c88194f5a482e0d
Luana Furtado <programacaolufrtd@gmail.com>
2023-04-30 16:21:57 +1200

    project univali extinction

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Amur leopard</h2>
+
+        <p>Poucas pessoas já viram um leopardo de Amur na natureza. Não é de surpreender, pois são tão poucos, mas é uma pena, considerando o quão bonitos são. Casacos grossos e exuberantes com anéis pretos e enormes caudas peludas que eles podem enrolar em si mesmos para se aquecer.</p>
+        <p>O leopardo de Amur é um animal noturno que vive e caça sozinho – principalmente nas vastas florestas da Rússia e da China. Durante o inverno rigoroso, os pelos dessa pelagem única podem crescer até 7 cm de comprimento.
+          Ao longo dos anos, o leopardo de Amur não foi apenas caçado impiedosamente, suas terras natais foram gradualmente destruídas pela exploração madeireira insustentável, incêndios florestais, construção de estradas, agricultura e desenvolvimento industrial.
+          Mas pesquisas recentes mostram que o trabalho de conservação está tendo um efeito positivo, e acredita-se que o número de leopardos selvagens de Amur tenha aumentado para pelo menos 120 adultos na Rússia e no nordeste da China. O leopardo de Amur, no entanto, continua sendo uma das subespécies de leopardo mais raras e criticamente ameaçadas do mundo.</p>
+      
+      <img src="https://www.wwf.org.uk/sites/default/files/styles/hero_m/public/2019-11/Small_WW272250.jpg?h=a11e816f&itok=k4A4peTi" alt="" />
       </header>
     </div>
   );
